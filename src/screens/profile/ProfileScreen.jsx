988446133b47fbb3e39feebed9f3a90bbb6a0afa
pablormacia <pr.macia@gmail.com>
2025-07-20 18:46:02 -0300

    Clase 8 - Código base en ProfileScreen con react-native-maps

--- src/screens/profile/ProfileScreen.jsx
+++ src/screens/profile/ProfileScreen.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Pressable, Image } from 'react-native'
+import { StyleSheet, Text, View, Pressable, Image,useWindowDimensions } from 'react-native'
 import { colors } from '../../global/colors'
 import CameraIcon from '../../components/CameraIcon'
 import { useState } from 'react'
@@ -6,15 +6,19 @@ import * as ImagePicker from 'expo-image-picker';
 import { useSelector,useDispatch } from 'react-redux';
 import { setProfileImage } from '../../features/user/userSlice';
 import { usePutProfileImageMutation } from '../../services/userService';
+import MapView, {Marker} from 'react-native-maps';
+
 
 
 const ProfileScreen = () => {
     //const [image, setImage] = useState("")
-    const [user, setUser] = useState("Demo")
+    //const [user, setUser] = useState("Demo")
     const dispatch = useDispatch()
     const image = useSelector(state=>state.userReducer.profileImage)
     const localId = useSelector(state=>state.userReducer.localId)
     const [triggerPutProfileImage, result] = usePutProfileImageMutation()
+
+    const user=useSelector(state=>state.userReducer.email)
 
     const pickImage = async () => {
         let result = await ImagePicker.launchCameraAsync({
@@ -51,6 +55,26 @@ const ProfileScreen = () => {
                 </Pressable>
             </View>
             <Text style={styles.profileData}>Email: {user}</Text>
+            <View style={styles.titleContainer}>
+                <Text>Mi última ubicación:</Text>
+            </View>
+            <View style={styles.mapContainer}>
+                <MapView
+                    style={styles.map}
+                    initialRegion={{
+                        latitude: 37.82172566127487,
+                        longitude:  -122.46896382718623,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}
+                >
+                    <Marker coordinate={{ "latitude": 37.82172566127487, "longitude": -122.46896382718623 }} title={"Lugar Geek"} />
+                </MapView>
+            </View>
+            <View style={styles.placeDescriptionContainer}>
+                <Text style={styles.mapTitle}>Mapa de ejemplo</Text>
+                <Text style={styles.address}>Dirección de ejemplo</Text>
+            </View>
         </View>
     )
 }
@@ -59,7 +83,7 @@ export default ProfileScreen
 
 const styles = StyleSheet.create({
     profileContainer: {
-        padding: 32,
+        paddingTop: 32,
         justifyContent: 'center',
         alignItems: 'center'
     },
@@ -88,5 +112,25 @@ const styles = StyleSheet.create({
         width: 128,
         height: 128,
         borderRadius: 128
+    },
+    titleContainer:{
+        marginVertical:16,
+    },
+    mapContainer: {
+      width: '100%',
+      height: 240,
+      overflow: "hidden",
+      elevation: 5,
+      marginBottom:16
+    },
+    map: {
+      height: 240,
+    },
+    mapTitle: {
+      fontWeight: '700'
+    },
+    placeDescriptionContainer:{
+        flexDirection:'row',
+        gap:16
     }
 })
