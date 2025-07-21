8a1a9b18229e28e6915ebc5083b24009b66a3548
pablormacia <pr.macia@gmail.com>
2025-07-21 19:31:07 -0300

    Clase 8 - Location y reverse Geododing + Api google

--- src/screens/profile/ProfileScreen.jsx
+++ src/screens/profile/ProfileScreen.jsx
@@ -1,24 +1,27 @@
-import { StyleSheet, Text, View, Pressable, Image,useWindowDimensions } from 'react-native'
+import { StyleSheet, Text, View, Pressable, Image, ActivityIndicator } from 'react-native'
 import { colors } from '../../global/colors'
 import CameraIcon from '../../components/CameraIcon'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import * as ImagePicker from 'expo-image-picker';
-import { useSelector,useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { setProfileImage } from '../../features/user/userSlice';
 import { usePutProfileImageMutation } from '../../services/userService';
-import MapView, {Marker} from 'react-native-maps';
-
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
 
 
 const ProfileScreen = () => {
     //const [image, setImage] = useState("")
     //const [user, setUser] = useState("Demo")
+    const [location, setLocation] = useState(null);
+    const [locationLoaded, setLocationLoaded] = useState(false);
+    const [address, setAddress] = useState("")
     const dispatch = useDispatch()
-    const image = useSelector(state=>state.userReducer.profileImage)
-    const localId = useSelector(state=>state.userReducer.localId)
+    const image = useSelector(state => state.userReducer.profileImage)
+    const localId = useSelector(state => state.userReducer.localId)
     const [triggerPutProfileImage, result] = usePutProfileImageMutation()
 
-    const user=useSelector(state=>state.userReducer.email)
+    const user = useSelector(state => state.userReducer.email)
 
     const pickImage = async () => {
         let result = await ImagePicker.launchCameraAsync({
@@ -36,9 +39,43 @@ const ProfileScreen = () => {
             //console.log(imgBase64)
             //setImage(result.assets[0].uri);
             dispatch(setProfileImage(imgBase64))
-            triggerPutProfileImage({localId: localId, image: imgBase64})
+            triggerPutProfileImage({ localId: localId, image: imgBase64 })
         }
     };
+
+    useEffect(() => {
+        async function getCurrentLocation() {
+            try {
+                let { status } = await Location.requestForegroundPermissionsAsync();
+                if (status !== 'granted') {
+                    console.log("Error al obtener los permisos")
+                    setLocationLoaded(true);
+                    return;
+                }
+
+                let location = await Location.getCurrentPositionAsync({});
+                if (location) {
+                    const response = await fetch(
+                        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${location.coords.latitude},${location.coords.longitude}&key=${process.env.EXPO_PUBLIC_API_MAPS}`
+                    );
+                    const data = await response.json()
+                    //console.log(data)
+                    setAddress(data.results[0].formatted_address)
+                    setLocation(location);
+                }
+            } catch (error) {
+                console.log("Error al obtener la ubicación:", error);
+            } finally {
+                setLocationLoaded(true);
+            }
+
+
+        }
+
+        getCurrentLocation();
+    }, []);
+
+    //console.log(location)
 
     return (
         <View style={styles.profileContainer}>
@@ -56,24 +93,37 @@ const ProfileScreen = () => {
             </View>
             <Text style={styles.profileData}>Email: {user}</Text>
             <View style={styles.titleContainer}>
-                <Text>Mi última ubicación:</Text>
+                <Text>Mi ubicación:</Text>
             </View>
             <View style={styles.mapContainer}>
-                <MapView
-                    style={styles.map}
-                    initialRegion={{
-                        latitude: 37.82172566127487,
-                        longitude:  -122.46896382718623,
-                        latitudeDelta: 0.0922,
-                        longitudeDelta: 0.0421,
-                    }}
-                >
-                    <Marker coordinate={{ "latitude": 37.82172566127487, "longitude": -122.46896382718623 }} title={"Lugar Geek"} />
-                </MapView>
+                {
+                    location
+                        ?
+                        <MapView
+                            style={styles.map}
+                            initialRegion={{
+                                latitude: location.coords.latitude,
+                                longitude: location.coords.longitude,
+                                latitudeDelta: 0.0922,
+                                longitudeDelta: 0.0421,
+                            }}
+                        >
+                            <Marker coordinate={{ "latitude": location.coords.latitude, "longitude": location.coords.longitude }} title={"Lugar Geek"} />
+                        </MapView>
+                        :
+                        (
+                            locationLoaded
+                            ?
+                            <Text>Hubo un problema al obtener la ubicación</Text>
+                            :
+                            <ActivityIndicator />
+                        )
+                        
+                }
+
             </View>
-            <View style={styles.placeDescriptionContainer}>
-                <Text style={styles.mapTitle}>Mapa de ejemplo</Text>
-                <Text style={styles.address}>Dirección de ejemplo</Text>
+            <View style={styles.addressContainer}>
+                <Text style={styles.address}>{address || ""}</Text>
             </View>
         </View>
     )
@@ -113,24 +163,26 @@ const styles = StyleSheet.create({
         height: 128,
         borderRadius: 128
     },
-    titleContainer:{
-        marginVertical:16,
+    titleContainer: {
+        marginVertical: 16,
     },
     mapContainer: {
-      width: '100%',
-      height: 240,
-      overflow: "hidden",
-      elevation: 5,
-      marginBottom:16
+        width: '100%',
+        height: 240,
+        overflow: "hidden",
+        elevation: 5,
+        marginBottom: 16
     },
     map: {
-      height: 240,
+        height: 240,
     },
     mapTitle: {
-      fontWeight: '700'
+        fontWeight: '700'
     },
-    placeDescriptionContainer:{
-        flexDirection:'row',
-        gap:16
+    addressContainer: {
+        alignItems: 'center',
+    },
+    address: {
+        textAlign: 'center',
     }
 })
